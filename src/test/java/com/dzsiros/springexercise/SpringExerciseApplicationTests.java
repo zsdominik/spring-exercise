82962a72168ba764df4c4e8b9f387171cc7a4f03
Dominik Zsiros <zsdominik@outlook.com>
2020-03-19 12:17:14 +0100

    init

---
+++ src/test/java/com/dzsiros/springexercise/SpringExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dzsiros.springexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringExerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
